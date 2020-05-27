25ab89de135b5b9a6a2f8fdfd67e9ce57a0e1640
DmitryY.Yaroshenko <DmitryY.Yaroshenko@dataart.com>
2020-05-27 21:21:53 +0300

    style main skills component

--- src/app/front/pages/main/main.component.ts
+++ src/app/front/pages/main/main.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class FrontMainComponent implements OnInit {
+  fpNav: HTMLElement;
   fullpageConfig: any;
   fullpageApi: any;
 
@@ -15,7 +16,21 @@ export class FrontMainComponent implements OnInit {
     this.fullpageConfig = {
       navigation: true,
       anchors: ['main-page', 'main-skills', 'experience-page'],
-      afterLoad: (origin, destination, direction) => { }
+      scrollOverflow: true,
+      afterRender: () => {
+       this.fpNav = document.querySelector('#fp-nav');
+      },
+      afterLoad: (origin: any, destination: any) => {
+        destination.item.classList.add('visited');
+      },
+      onLeave: (origin: any, destination: any) => {
+        if (destination.index === 0) {
+          this.fpNav.classList.remove('dark');
+          return;
+        }
+
+        this.fpNav.classList.add('dark');
+      }
     };
   }
 
